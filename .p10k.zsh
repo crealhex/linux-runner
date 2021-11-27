2fa46b31d95d281f0ed5c230e0d86f8d22ac8105
Luis Enco <encoluis@hotmail.com>
2021-11-27 03:06:42 -0500

    Add personal configurations
    - .p10k.zsh
    - .zshrc

--- .p10k.zsh
+++ .p10k.zsh
@@ -206,12 +206,12 @@
 
   ##################################[ dir: current directory ]##################################
   # Current directory background color.
-  typeset -g POWERLEVEL9K_DIR_BACKGROUND=4
+  typeset -g POWERLEVEL9K_DIR_BACKGROUND=90
   # Default current directory foreground color.
   typeset -g POWERLEVEL9K_DIR_FOREGROUND=254
   # If directory is too long, shorten some of its segments to the shortest possible unique
   # prefix. The shortened directory can be tab-completed to the original.
-  typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
+  typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_last
   # Replace removed segment suffixes with this symbol.
   typeset -g POWERLEVEL9K_SHORTEN_DELIMITER=
   # Color of the shortened directory segments.
@@ -345,9 +345,9 @@
 
   #####################################[ vcs: git status ]######################################
   # Version control system colors.
-  typeset -g POWERLEVEL9K_VCS_CLEAN_BACKGROUND=2
+  typeset -g POWERLEVEL9K_VCS_CLEAN_BACKGROUND=72
   typeset -g POWERLEVEL9K_VCS_MODIFIED_BACKGROUND=3
-  typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND=2
+  typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND=yellow
   typeset -g POWERLEVEL9K_VCS_CONFLICTED_BACKGROUND=3
   typeset -g POWERLEVEL9K_VCS_LOADING_BACKGROUND=8
 
--- runner.sh
+++ runner.sh
@@ -28,9 +28,10 @@ ${GREEN}Running terminal configurations...${RESET}
 EOF
 
 # Configuring zsh installer vars
-# RUNZSH=no
-# CHSH=no
-# KEEP_ZSHRC=no
+# RUNZSH=${RUNZSH:-no}
+# CHSH=${CHSH:-no}
+# KEEP_ZSHRC=${KEEP_ZSHRC:-no}
+
 # Installing zsh package
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
@@ -43,11 +44,27 @@ sed -ri 's/(ZSH_THEME=")([a-zA-Z]+)(")/\1powerlevel10k\/powerlevel10k\3/g' ~/.zs
 
 # Change default shell to zsh
 chsh -s $(which zsh)
-exec zsh
+# exec zsh
 
-echo "${GREEN}Now restart your terminal${RESET}"
+# Ubuntu-Runner settings
+RUNNER=${RUNNER:-~/.linux-runner}
+REPO=${REPO:-crealhex/linux-runner}
+REMOTE=${REMOTE:-https://github.com/${REPO}.git}
+BRANCH=${BRANCH:-master}
 
-# Running zsh
-# /usr/bin/zsh
+git clone -c core.eol=lf -c core.autocrlf=false \
+    -c fsck.zeroPaddedFilemode=ignore \
+    -c fetch.fsck.zeroPaddedFilemode=ignore \
+    -c receive.fsck.zeroPaddedFilemode=ignore \
+    -c linux-runner.remote=origin \
+    -c linux-runner.branch="$BRANCH" \
+    --depth=1 --branch "$BRANCH" "$REMOTE" "$RUNNER"
 
-# cp /mnt/c/Users/warender/Desktop/ubuntu-runner/packager.sh .
+cp ~/.linux-runner/.p10k.zsh ~/
+cp ~/.linux-runner/.zshrc ~/
+
+# Run Powerlevel10k configuration wizard
+
+# p10k configure
+
+# cp /mnt/c/Users/warender/Desktop/ubuntu-runner/runner.sh .
